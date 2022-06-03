c4aad758b9090eca00f71cc1ad0793e88d6f8cc1
Pierce <ppuckette@gmail.com>
2022-06-02 22:17:29 -0400

    proper list and key prop

--- src/index.js
+++ src/index.js
@@ -7,37 +7,46 @@ import { createRoot } from "react-dom/client";
 //declare vars
 const books = [
 	{
+		id: 1001,
 		img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg",
 		title: "I Love You to the Movie and Back",
 		author: "Amelia Hepworth",
 	},
 
 	{
+		id: 1002,
 		img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg",
 		title: "Our Class is a Family",
 		author: "Shannon Olsen",
 	},
+	{
+		id: 1003,
+		img: "https://m.media-amazon.com/images/I/71gGPRRlyTL._AC_UL320_.jpg",
+		title: "The Vanishing Half: A Novel",
+		author: "Brit Bennett",
+	},
 ];
 
 
-const names = ["john", "peter", "susan"];
-const newName = names.map((name) => {
-	return <h1>{name}</h1>;
-});
 
-console.log(newName);
 
 const BookList = () => {
 	return (
 		<section className="booklist">
-			{newName}
+			{books.map((book) => {
+				//destructure items on book array
+				
+				return (
+					<Book key={book.id} book={book}></Book>
+				);
+			})}
 		</section>
 	);
 };
 
 const Book = (props) => {
 	//use special prop called children to show book description
-	const { img, title, author } = props;
+	const { img, title, author } = props.book;
 	return (
 		<article className="book">
 			<img src={img} alt="Childrens Book" />
